06c6345fbb67bbc471516280f67859bde0066951
鱼广洲 <yuguangzhou@zjjcnt.com>
2022-02-07 17:18:39 +0800

    first commit

--- build/base.config.js
+++ build/base.config.js
@@ -1,15 +1,8 @@
-/*
- * @Author: yuguangzhou
- * @Date: 2022-01-19 11:29:41
- * @LastEditTime: 2022-02-03 13:27:48
- * @LastEditors: Please set LastEditors
- * @Description: vite基础配置
- */
+
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import MarkDown from 'vite-plugin-md'
-// https://vitejs.dev/config/
 
 export default defineConfig({
   base: "./",
--- build/doc.config.js
+++ build/doc.config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: yuguangzhou
  * @Date: 2022-01-21 16:41:38
- * @LastEditTime: 2022-01-25 15:10:03
- * @LastEditors: yuguangzhou
+ * @LastEditTime: 2022-02-07 15:09:45
+ * @LastEditors: Please set LastEditors
  * @Description: 文档配置文件
  */
 import baseConfig from './base.config';
